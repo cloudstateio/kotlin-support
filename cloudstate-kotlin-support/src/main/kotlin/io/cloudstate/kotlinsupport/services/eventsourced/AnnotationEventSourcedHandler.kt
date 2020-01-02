3c9d3c3325206c8ba26caec916c9085fb58e4e92
Adriano Santos <sleipnir@bsd.com.br>
2020-01-02 18:31:34 -0300

    Add reflection cache

--- cloudstate-kotlin-support/src/main/kotlin/io/cloudstate/kotlinsupport/services/eventsourced/AnnotationEventSourcedHandler.kt
+++ cloudstate-kotlin-support/src/main/kotlin/io/cloudstate/kotlinsupport/services/eventsourced/AnnotationEventSourcedHandler.kt
@@ -4,8 +4,13 @@ import io.cloudstate.kotlinsupport.annotations.CommandHandler
 import io.cloudstate.kotlinsupport.annotations.EventHandler
 import io.cloudstate.kotlinsupport.annotations.Snapshot
 import io.cloudstate.kotlinsupport.annotations.SnapshotHandler
+import java.lang.reflect.Method
 
 class AnnotationEventSourcedHandler(private val eventSourcedEntity: AnnotationEventSourcedEntity): EventSourcedEntityHandlerContext {
+    private val eventHandlers: MutableMap<String, MethodInvocable> = mutableMapOf()
+    private val commandHandlers: MutableMap<String, MethodInvocable> = mutableMapOf()
+    private val snapshotHandlers: MutableMap<String, MethodInvocable> = mutableMapOf()
+    private val snapshotsHandlers: MutableMap<String, MethodInvocable> = mutableMapOf()
 
     val annotations: List<Annotation> = eventSourcedEntity.javaClass.annotations.filter {
         it is CommandHandler || it is EventHandler || it is Snapshot || it is SnapshotHandler
@@ -13,17 +18,21 @@ class AnnotationEventSourcedHandler(private val eventSourcedEntity: AnnotationEv
 
     init {
         // process annotations in eventSourcedEntity
+        cachedMethods()
     }
 
     override fun handleEvent(event: com.google.protobuf.Any?) {
+        val typeUrl = event?.typeUrl
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun handleCommand(command: com.google.protobuf.Any?): com.google.protobuf.Any? {
+        val typeUrl = command?.typeUrl
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun handleSnapshot(snapshot: com.google.protobuf.Any?) {
+        val typeUrl = snapshot?.typeUrl
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
@@ -38,4 +47,42 @@ class AnnotationEventSourcedHandler(private val eventSourcedEntity: AnnotationEv
     override fun forward(to: Any) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
+
+    private fun cachedMethods() {
+        this.eventSourcedEntity.javaClass.methods.forEach { method ->
+            when {
+                method.isAnnotationPresent(CommandHandler::class.java) -> {
+                    val parameterType: Class<*> = method.parameterTypes[0]
+                    val parameterTypeName: String = parameterType.simpleName
+                    commandHandlers[parameterTypeName] = MethodInvocable(parameterTypeName, method, parameterType)
+                }
+            }
+
+            when {
+                method.isAnnotationPresent(EventHandler::class.java) -> {
+                    val parameterType: Class<*> = method.parameterTypes[0]
+                    val parameterTypeName: String = parameterType.simpleName
+                    eventHandlers[parameterTypeName] = MethodInvocable(parameterTypeName, method, parameterType)
+                }
+            }
+
+            when {
+                method.isAnnotationPresent(SnapshotHandler::class.java) -> {
+                    val parameterType: Class<*> = method.parameterTypes[0]
+                    val parameterTypeName: String = parameterType.simpleName
+                    snapshotHandlers[parameterTypeName] = MethodInvocable(parameterTypeName, method, parameterType)
+                }
+            }
+
+            when {
+                method.isAnnotationPresent(Snapshot::class.java) -> {
+                    val parameterType: Class<*> = method.parameterTypes[0]
+                    val parameterTypeName: String = parameterType.simpleName
+                    snapshotsHandlers[parameterTypeName] = MethodInvocable(parameterTypeName, method, parameterType)
+                }
+            }
+        }
+    }
 }
+
+data class MethodInvocable(val name: String, val method: Method, val parameterType: Class<*>)
