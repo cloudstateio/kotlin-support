641cf0d9fcab744fc110d76f1621f85d5e11e8e9
Adriano Santos <sleipnir@bsd.com.br>
2020-04-18 20:20:41 -0300

    Fixe issue #13 and minor changes

--- cloudstate-kotlin-support/src/test/kotlin/io/cloudstate/kotlinsupport/tests/CrdtTranscoderTest.kt
+++ cloudstate-kotlin-support/src/test/kotlin/io/cloudstate/kotlinsupport/tests/CrdtTranscoderTest.kt
@@ -5,6 +5,7 @@ import io.cloudstate.javasupport.EntityId
 import io.cloudstate.kotlinsupport.logger
 import io.cloudstate.kotlinsupport.api.crdt.*
 import io.cloudstate.kotlinsupport.transcoding.CrdtTranscoder
+import net.bytebuddy.agent.ByteBuddyAgent
 
 import org.junit.Test
 import java.lang.reflect.Constructor
@@ -16,6 +17,11 @@ class CrdtTranscoderTest {
 
     private val log = logger()
 
+    init {
+        log.debug("Initializing ByteBuddy Agent....")
+        ByteBuddyAgent.install()
+    }
+
     @Test
     fun `List Names of Methods in Log`() {
         val transcoder = CrdtTranscoder(CrdtTestEntity::class.java)
@@ -24,16 +30,16 @@ class CrdtTranscoderTest {
         val ctor: Constructor<*>? = targetClazzRepresentation?.getConstructor(String::class.java)
         val entityInstance: CrdtTestEntity = ctor?.newInstance("entityId") as CrdtTestEntity
 
-        var crdtEntity = entityInstance.javaClass.getAnnotation(JCrdtEntity::class.java)
+        val crdtEntity = entityInstance.javaClass.getAnnotation(JCrdtEntity::class.java)
 
         log.info("Entity type is not null. Type is ${crdtEntity?.javaClass?.simpleName}")
         assertNotNull(entityInstance)
 
-        entityInstance?.javaClass?.annotations.forEach {
+        entityInstance.javaClass.annotations.forEach {
             log.info("Test Class annotation found. $it")
         }
 
-        entityInstance?.javaClass?.methods.forEach {
+        entityInstance.javaClass.methods.forEach {
             it.annotations.forEach { annotation ->
                 log.info("Found annotation $annotation in ${it.name}")
             }
@@ -48,7 +54,7 @@ class CrdtTranscoderTest {
 }
 
 @CrdtEntity
-class CrdtTestEntity(@param:EntityId private val entityId: String) {
+private class CrdtTestEntity(@param:EntityId private val entityId: String) {
 
     @CommandHandler(name = "otherGetCart")
     fun getCart(){}
