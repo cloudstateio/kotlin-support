40496fa9f6a12dd0ddd5fbd369fad4642091a4cb
Adriano Santos <sleipnir@bsd.com.br>
2020-04-20 17:59:39 -0300

    Add snapshots mappings

--- cloudstate-kotlin-support/src/main/kotlin/io/cloudstate/kotlinsupport/api/eventsourced/KotlinEventBehaviorReflection.kt
+++ cloudstate-kotlin-support/src/main/kotlin/io/cloudstate/kotlinsupport/api/eventsourced/KotlinEventBehaviorReflection.kt
@@ -5,6 +5,8 @@ import io.cloudstate.javasupport.impl.ResolvedServiceMethod
 import io.cloudstate.kotlinsupport.ReflectionHelper
 import io.cloudstate.kotlinsupport.annotations.eventsourced.CommandHandler
 import io.cloudstate.kotlinsupport.annotations.eventsourced.EventHandler
+import io.cloudstate.kotlinsupport.annotations.eventsourced.Snapshot
+import io.cloudstate.kotlinsupport.annotations.eventsourced.SnapshotHandler
 import java.lang.reflect.Method
 import java.util.*
 
@@ -47,12 +49,24 @@ class KotlinEventBehaviorReflection(private val entityClass: Class<*>, private v
                             KotlinAnnotationBasedEventSourced.CommandHandlerInvoker(reflectionHelper.ensureAccessible(method),serviceMethod, reflectionHelper))
                 }.toMap()
 
-    private fun getSnapshotInvoker(): Optional<KotlinAnnotationBasedEventSourced.SnapshotInvoker> {
-        TODO("Not yet implemented")
-    }
+    private fun getSnapshotHandlers(): Map<Class<*>, KotlinAnnotationBasedEventSourced.SnapshotHandlerInvoker> =
+        allMethods
+                .filter { it.isAnnotationPresent(SnapshotHandler::class.java) }
+                .map { method -> Pair(
+                        getSnapshotHandlerClass(method, method.getAnnotation(SnapshotHandler::class.java)),
+                        KotlinAnnotationBasedEventSourced.SnapshotHandlerInvoker(reflectionHelper.ensureAccessible(method), reflectionHelper)) }
+                .toMap()
 
-    private fun getSnapshotHandlers(): Map<Class<*>, KotlinAnnotationBasedEventSourced.SnapshotHandlerInvoker> {
-        TODO("Not yet implemented")
+    private fun getSnapshotInvoker(): Optional<KotlinAnnotationBasedEventSourced.SnapshotInvoker> {
+        val map = allMethods
+                .filter { it.isAnnotationPresent(Snapshot::class.java) }
+                .map { method -> KotlinAnnotationBasedEventSourced.SnapshotInvoker(reflectionHelper.ensureAccessible(method), reflectionHelper) }
+
+        if (map.size > 1) {
+            //Todo: Better message here
+            throw RuntimeException("Multiple snapshoting methods found on Entity function")
+        }
+        return Optional.of(map[0])
     }
 
     private fun getEventClass(method: Method, annotation: Annotation): Class<*> {
@@ -69,6 +83,32 @@ class KotlinEventBehaviorReflection(private val entityClass: Class<*>, private v
         if ((parameter.javaClass != evtAnn.eventClass.java) && (evtAnn.eventClass.java != Object::class.java) ) {
             return evtAnn.eventClass.java
         }
+        return parameter.javaClass
+    }
+
+    private fun getSnapshotHandlerClass(method: Method, annotation: Annotation): Class<*> {
+        val evtAnn = annotation as SnapshotHandler
+
+        if (method.parameters.filter { param -> !param.type.isInstance(Context::class.java)  }
+                        .size > 1) {
+            throw  RuntimeException("Event handling method ${method.name} cannot contain more than one type parameter. " +
+                    "Parameters ${method.parameters}")
+        }
+
+        val parameter = method.parameters.filter { param -> !param.type.isInstance(Context::class.java) }[0]
+        return parameter.javaClass
+    }
+
+    private fun getSnapshotClass(method: Method, annotation: Annotation): Class<*> {
+        val evtAnn = annotation as Snapshot
+
+        if (method.parameters.filter { param -> !param.type.isInstance(Context::class.java)  }
+                        .size > 1) {
+            throw  RuntimeException("Event handling method ${method.name} cannot contain more than one type parameter. " +
+                    "Parameters ${method.parameters}")
+        }
+
+        val parameter = method.parameters.filter { param -> !param.type.isInstance(Context::class.java) }[0]
         return parameter.javaClass
     }
 
