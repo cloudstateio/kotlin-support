6127fde89bbe3454a18eb23fd0a1383de604cb3d
Vladimir Korenev <vkorenev@gmail.com>
2020-04-23 21:34:11 -0700

    Substitute dependency on "io.cloudstate:cloudstate-kotlin-support"

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,13 @@
 allprojects {
     group = "io.cloudstate"
     version = "0.4.3"
+
+    configurations.all {
+        resolutionStrategy {
+            dependencySubstitution {
+                substitute(module("io.cloudstate:cloudstate-kotlin-support"))
+                        .with(project(":cloudstate-kotlin-support"))
+            }
+        }
+    }
 }
